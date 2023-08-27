6252235ce419858e0768977dba7d4922202dc5aa
Rico Pahlefi <101618011+ricopahlefi22@users.noreply.github.com>
2023-08-28 06:58:59 +0700

    menghindari human error

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,20 +13,5 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Rico Pahlefi',
-            'email' => 'ricopahlefi22@gmail.com',
-            'phone_number' => '089528597031',
-            'address' => 'Jl. Poros Ketapang - Siduk KM. 9 Kabupaten Ketapang',
-            'password' => bcrypt('MudahDitebak123!'),
-        ]);
-
-        User::factory()->create([
-            'name' => 'Vika Khairunisa',
-            'email' => 'vika.khairunisa08@gmail.com',
-            'phone_number' => '089522071515',
-            'address' => 'Jl. Poros Ketapang - Siduk KM. 20 Kabupaten Ketapang',
-            'password' => bcrypt('12341234'),
-        ]);
     }
 }
